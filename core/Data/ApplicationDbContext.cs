b56012a3cb0deaa58e0ed0d052842f4b28c1efaa
Illia-creator <sie29092002@gmail.com>
2022-12-17 00:33:42 +0200

    is feat: create project

---
+++ core/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace core.Data
+{
+    public class https ://github.com/Illia-creator/MonitorShop.git : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
